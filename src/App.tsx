426738c639c8bf6ea8e64b0c40403b55279e31ab
Ekaterina Bondareva <1978231@bk.ru>
2024-06-03 22:54:33 +0500

    added information component

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,38 @@ interface UserData {
   name: string;
   repos: number;
 }
+interface RepoData {
+  name: string;
+  stars: number;
+}
+
 export function App() {
   const [givenValue, setGivenValue] = useState<string>("");
   const [selectorValue, setSelectorValue] = useState<string>("user");
 
   const [userData, setUserData] = useState<UserData | undefined>();
-  console.log(userData);
+  const [repoData, setRepoData] = useState<RepoData | undefined>();
+
+  const [isSendData, setIsSendData] = useState<boolean>(false);
+
   return (
     <div className="App">
       <Selector setSelectorValueFunction={setSelectorValue} />
 
       <Input
         givenValueInput={givenValue}
+        selectorValueInput={selectorValue}
         setGivenValueFunction={setGivenValue}
         setUserDataFunction={setUserData}
+        setRepoDataFunction={setRepoData}
+        setIsSendDataFunction={setIsSendData}
+      />
+
+      <Information
+        userDataValue={userData}
+        repoDataValue={repoData}
+        isSendDataValue={isSendData}
+        selectorValueInformation={selectorValue}
       />
     </div>
   );
@@ -59,16 +77,24 @@ const Selector: React.FC<PropsSelector> = ({ setSelectorValueFunction }) => {
 
 interface PropsInput {
   givenValueInput: string;
+  selectorValueInput: string;
   setGivenValueFunction: React.Dispatch<React.SetStateAction<string>>;
   setUserDataFunction: React.Dispatch<
     React.SetStateAction<UserData | undefined>
   >;
+  setRepoDataFunction: React.Dispatch<
+    React.SetStateAction<RepoData | undefined>
+  >;
+  setIsSendDataFunction: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const Input: React.FC<PropsInput> = ({
   givenValueInput,
+  selectorValueInput,
   setGivenValueFunction,
   setUserDataFunction,
+  setRepoDataFunction,
+  setIsSendDataFunction,
 }) => {
   return (
     <>
@@ -77,20 +103,66 @@ const Input: React.FC<PropsInput> = ({
       ></input>
       <button
         onClick={() => {
-          fetch(`https://api.github.com/users/${givenValueInput}`)
-            .then((response) => {
-              return response.json();
-            })
-            .then((data: any) => {
-              setUserDataFunction({
-                name: `${data.name}`,
-                repos: data.public_repos,
+          if (selectorValueInput === "user") {
+            fetch(`https://api.github.com/users/${givenValueInput}`)
+              .then((response) => {
+                return response.json();
+              })
+              .then((data: any) => {
+                setUserDataFunction({
+                  name: `${data.name}`,
+                  repos: data.public_repos,
+                });
               });
-            });
+          } else {
+            fetch(`https://api.github.com/repos/${givenValueInput}`)
+              .then((response) => {
+                return response.json();
+              })
+              .then((data: any) => {
+                setRepoDataFunction({
+                  name: `${data.full_name}`,
+                  stars: data.stargazers_count,
+                });
+              });
+          }
+          setIsSendDataFunction(true);
         }}
       >
         Send
       </button>
     </>
+  );
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+interface PropsInformation {
+  userDataValue: UserData | undefined;
+  repoDataValue: RepoData | undefined;
+  isSendDataValue: boolean;
+  selectorValueInformation: string;
+}
+
+const Information: React.FC<PropsInformation> = ({
+  userDataValue,
+  repoDataValue,
+  isSendDataValue,
+  selectorValueInformation,
+}) => {
+  return isSendDataValue ? (
+    selectorValueInformation === "user" ? (
+      <div>
+        <div>Full Name: {userDataValue?.name}</div>
+        <div>Count Of Repos: {userDataValue?.repos}</div>
+      </div>
+    ) : (
+      <div>
+        <div>Full Name: {repoDataValue?.name}</div>
+        <div>Count Of Repos: {repoDataValue?.stars}</div>
+      </div>
+    )
+  ) : (
+    <div></div>
   );
 };
